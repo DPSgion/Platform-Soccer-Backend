1492c02556c6d8eebfa0c5af7462a34d78e1a425
Pham Huu Doi <doipham1812@gmail.con>
2026-04-11 17:15:07 +0700

    repair  filename

--- src/modules/dashboardPublic/dashboardPublicRoute.js
+++ src/modules/dashboardPublic/dashboardPublicRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("./dashboardPublic.controller");
+const controller = require("./dashboardPublicController");
 
 router.get("/tournament", controller.getTournaments);
 router.get("/tournament/:id/match", controller.getTournamentMatches);
