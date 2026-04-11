e887ca6589ecdcb0b2e0fa844ae2d9da695c0198
Pham Huu Doi <doipham1812@gmail.con>
2026-04-11 16:36:29 +0700

    add api public/tournament/:id/match

--- src/modules/dashboardPublic/dashboardPublic.route.js
+++ src/modules/dashboardPublic/dashboardPublic.route.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const controller = require("./dashboardPublic.controller");
 
 router.get("/tournament", controller.getTournaments);
-
+router.get("/tournament/:id/match", controller.getTournamentMatches);
 module.exports = router;
