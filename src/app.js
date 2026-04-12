e2b4e8aa6986f3df9b8dc2a5477dfc5f31809a4e
DH52200637-NguyenTriHao <haonguyen27042004@gmail.com>
2026-04-12 21:11:35 +0700

    feat(tournament): test database

--- src/app.js
+++ src/app.js
@@ -7,21 +7,19 @@ const config = require("./dbConfig");
 const connection = mysql.createConnection(config);
 app.use(cors());
 app.use(express.json());
-
-connection.connect(err => {
-    if (err) {
-        console.error('Lỗi kết nối Local DB: ' + err.message);
-        return;
-    }
-    console.log('Ngon! Đã kết nối thành công tới: ' + config.database);
-
+app.get("/test-db", (req, res) => {
     connection.query('SELECT * FROM users', (err, results) => {
         if (err) {
-            console.error('Lỗi truy vấn: ' + err.message);
-        } else {
-            console.log('Dữ liệu từ database của Hào nè:');
-            console.table(results);
+            return res.status(500).json({
+                success: false,
+                error: err.message
+            });
         }
+        res.json({
+            success: true,
+            description: "Dữ liệu từ database của Hào",
+            data: results
+        });
     });
 });
 
