57046aac557649ab977d465716813a2628468c9b
Nguyen Dai Tai <daitai21062004@gmail.com>
2026-04-18 20:09:35 +0700

    feat: add file upload for create/update team with OCI

--- src/modules/teams/teamRoutes.js
+++ src/modules/teams/teamRoutes.js
@@ -6,15 +6,11 @@ const teamController = require("./teamController");
 const { authMiddleware } = require("../../middlewares/authMiddleware");
 
 //TEAM
-router.post("/", authMiddleware(["ORGANIZER"]), teamController.createTeam);
+router.post("/", authMiddleware(["ORGANIZER"]), upload.any(), teamController.createTeam);
 router.get("/", authMiddleware(["ORGANIZER"]), teamController.getAllTeams);
 router.get("/:teamId", teamController.getTeamById);
-router.put("/:teamId", authMiddleware(["ORGANIZER"]), teamController.updateTeam);
+router.put("/:teamId", authMiddleware(["ORGANIZER"]), upload.fields([{name: 'logo', maxCount: 1}, {name: 'kit', maxCount: 5}]), teamController.updateTeam);
 router.delete("/:teamId", authMiddleware(["ORGANIZER"]), teamController.deleteTeam);
-
-//UPLOAD FILE
-router.post("/:teamId/logo", authMiddleware(["ORGANIZER"]), upload.single("logo"), teamController.uploadLogo);
-router.post("/:teamId/kit", authMiddleware(["ORGANIZER"]), upload.array("kit", 5), teamController.uploadKit);
 
 //TEAM MEMBERS
 router.get("/:teamId/members", teamController.getTeamMembers);
