4d37e052ad2b3882578df926f02a559a94ae4dad
Nguyen Dai Tai <daitai21062004@gmail.com>
2026-04-18 18:00:24 +0700

    feat: add team API

--- src/modules/teams/teamController.js
+++ src/modules/teams/teamController.js
@@ -1,6 +1,5 @@
 const teamService = require("./teamService");
 const { AppError } = require("../../middlewares/errorMiddleware");
-//const { uploadFileToOCI } = require('../../utils/ociUpload');
 
 // CREATE
 const createTeam = async (req, res, next) => {
@@ -186,29 +185,6 @@ const getTeamMemberById = async (req, res, next) => {
     console.error("Error in getTeamMemberById:", error);
     next(error);
   }
-};
-
-// uploadLogo
-const uploadLogo = async (req, res, next) => {
-  try {
-    const { teamId } = req.params;
-    const logoUrl = await uploadFileToOCI(req.file);
-    await teamService.updateTeam(teamId, { logo_url: logoUrl, manager_id: req.user.id });
-    return res.status(200).json({ success: true, message: "Logo uploaded", data: { logo_url: logoUrl } });
-  } catch (error) { return next(error); }
-};
-
-// uploadKit
-const uploadKit = async (req, res, next) => {
-  try {
-    const { teamId } = req.params;
-    const kitUrls = await Promise.all(req.files.map(f => uploadFileToOCI(f)));
-    const existingTeam = await teamService.getTeamById(teamId);
-    const existingKits = existingTeam.kit_url || [];
-    const newKits = [...existingKits, ...kitUrls];
-    await teamService.updateTeam(teamId, { kit_url: JSON.stringify(newKits), manager_id: req.user.id });
-    return res.status(200).json({ success: true, message: "Kit uploaded", data: { kit_url: newKits } });
-  } catch (error) { return next(error); }
 };
 
 module.exports = {
