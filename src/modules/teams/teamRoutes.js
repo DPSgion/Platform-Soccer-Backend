b5a6ce9ff7fd18b1bd38cc1772f7224df1ecce8e
Nguyen Dai Tai <daitai21062004@gmail.com>
2026-04-18 18:51:34 +0700

    feat: add upload logo/kit for team with OCI

--- src/modules/teams/teamRoutes.js
+++ src/modules/teams/teamRoutes.js
@@ -12,6 +12,10 @@ router.get("/:teamId", teamController.getTeamById);
 router.put("/:teamId", authMiddleware(["ORGANIZER"]), teamController.updateTeam);
 router.delete("/:teamId", authMiddleware(["ORGANIZER"]), teamController.deleteTeam);
 
+//UPLOAD FILE
+router.post("/:teamId/logo", authMiddleware(["ORGANIZER"]), upload.single("logo"), teamController.uploadLogo);
+router.post("/:teamId/kit", authMiddleware(["ORGANIZER"]), upload.array("kit", 5), teamController.uploadKit);
+
 //TEAM MEMBERS
 router.get("/:teamId/members", teamController.getTeamMembers);
 router.get("/:teamId/members/:playerId", teamController.getTeamMemberById);
