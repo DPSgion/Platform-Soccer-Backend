1b0c2e721f2bf4bbf790dde1cada3d19283c1d4b
DH52200637-NguyenTriHao <haonguyen27042004@gmail.com>
2026-04-20 20:55:01 +0700

    feat(team-api-daitai): delete 1 route duplicate

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
+const matchRoutes = require("../modules/matches/matchRoutes");
 const tournamentRoutes = require("../modules/tournaments/tournamentRoutes");
 const userRoutes = require("../modules/users/userRoutes");
 const dashboardPublicRoutes = require("../modules/dashboardPublic/dashboardPublicRoute");
 const teamRoutes = require("../modules/teams/teamRoutes");
 const authRoutes = require("../modules/auth/authRoutes");
-const matchRoutes = require("../modules/matches/matchRoutes");
 
 router.use("/tournaments", tournamentRoutes);
 router.use("/users", userRoutes);
