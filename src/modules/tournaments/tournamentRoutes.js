446e0c228032e672c9e62da7a73845790ea0d3ea
DH52200637-NguyenTriHao <haonguyen27042004@gmail.com>
2026-04-25 10:59:33 +0700

    resolve stash conflict

--- src/modules/tournaments/tournamentRoutes.js
+++ src/modules/tournaments/tournamentRoutes.js
@@ -1,17 +1,17 @@
-
 const express = require("express");
 const router = express.Router();
 
 const controller = require("./tournamentController");
 const { authMiddleware } = require("../../middlewares/authMiddleware");
 
-//Get all tournament
+// Get all tournaments
 router.get("/", authMiddleware(["ORGANIZER"]), controller.getAllTournaments);
+
 // Create tournament
-router.post("/create", controller.createTournament);
+router.post("/create", authMiddleware(["ORGANIZER"]), controller.createTournament);
 
 // Update tournament
-router.put("/:id/update", controller.updateTournament);
+router.put("/:id/update", authMiddleware(["ORGANIZER"]), controller.updateTournament);
 
 // Delete tournament
 router.delete("/:id/delete", authMiddleware(["ORGANIZER"]), controller.deleteTournament);
